af4bf986d5aeccb9c5da7ce1b80c6cea28f86d9a
Benjamin Piouffle <benjamin@opencollective.com>
2020-09-01 00:24:47 +0200

    enhancement: Use  from env

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,11 @@ import CaptainFactDiscordClient from "./lib/captain_fact_discord_client";
 new CaptainFactDiscordClient().start().then(
   () => {
     // Start a web server to satisfy Zeit
-    logInfo("Starting web server on port 3000");
-    http.createServer().listen(3000);
+    logInfo(`Starting web server on port ${process.env.PORT}`);
+    http.createServer().listen(process.env.PORT);
   },
   () => {
     logError("Invalid Discord API token");
     process.exit(1);
-  },
+  }
 );
